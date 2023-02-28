f70754f25e4241674f7114963f8e6069de552d7d
Lucas Claros <lucasclaros@tokenlab.com.br>
2023-02-28 17:47:30 -0300

    RGB Switch Picker

---
+++ ColorPickerApp/ColorPickerApp/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  ColorPickerApp
+//
+//  Created by Lucas da Silva Claros on 28/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var redSwitch: UISwitch!
+    @IBOutlet weak var greenSwitch: UISwitch!
+    @IBOutlet weak var blueSwitch: UISwitch!
+    @IBOutlet weak var colorView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateColor()
+    }
+
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        updateColor()
+    }
+    
+    func updateColor() {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        
+        if redSwitch.isOn {
+            red = 1
+        }
+        if greenSwitch.isOn {
+            green = 1
+        }
+        if blueSwitch.isOn {
+            blue = 1
+        }
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        colorView.backgroundColor = color
+    }
+}
+
