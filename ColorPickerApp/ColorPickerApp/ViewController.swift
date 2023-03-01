146df6d21037b65bf19b211848f334b95dd1797e
Lucas Claros <lucasclaros@tokenlab.com.br>
2023-03-01 03:51:01 -0300

    Implemented some customization with sliders and changed theme

--- ColorPickerApp/ColorPickerApp/ViewController.swift
+++ ColorPickerApp/ColorPickerApp/ViewController.swift
@@ -15,12 +15,50 @@ class ViewController: UIViewController {
     @IBOutlet weak var blueSwitch: UISwitch!
     @IBOutlet weak var colorView: UIView!
     
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var resetButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        customSwitches()
+            
+        colorView.layer.borderWidth = 3
+        colorView.layer.cornerRadius = 10
+        colorView.layer.borderColor = UIColor.black.cgColor
+        
+        resetButton.layer.borderWidth = 1
+        resetButton.layer.borderColor = UIColor.red.cgColor
+        resetButton.layer.cornerRadius = 5
+        
+        updateSlidersControl()
         updateColor()
     }
 
     @IBAction func switchChanged(_ sender: UISwitch) {
+        
+        updateSlidersControl()
+        updateColor()
+    }
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        
+        updateSlidersControl()
+        updateColor()
+    }
+    
+    @IBAction func reset(_ sender: UIButton) {
+        redSlider.value = 1
+        greenSlider.value = 1
+        blueSlider.value = 1
+        
+        redSwitch.isOn = false
+        greenSwitch.isOn = false
+        blueSwitch.isOn = false
+        
+        updateSlidersControl()
         updateColor()
     }
     
@@ -30,17 +68,32 @@ class ViewController: UIViewController {
         var blue: CGFloat = 0
         
         if redSwitch.isOn {
-            red = 1
+            red = CGFloat(redSlider.value)
         }
         if greenSwitch.isOn {
-            green = 1
+            green = CGFloat(greenSlider.value)
         }
         if blueSwitch.isOn {
-            blue = 1
+            blue = CGFloat(blueSlider.value)
         }
         
         let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
         colorView.backgroundColor = color
+    }
+    
+    func updateSlidersControl() {
+        redSlider.isEnabled = redSwitch.isOn
+        greenSlider.isEnabled = greenSwitch.isOn
+        blueSlider.isEnabled = blueSwitch.isOn
+    }
+    
+    func customSwitches() {
+        redSwitch.backgroundColor = .white
+        redSwitch.layer.cornerRadius = 16
+        greenSwitch.backgroundColor = .white
+        greenSwitch.layer.cornerRadius = 16
+        blueSwitch.backgroundColor = .white
+        blueSwitch.layer.cornerRadius = 16
     }
 }
 
